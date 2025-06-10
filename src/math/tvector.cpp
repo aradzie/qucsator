6dd303e69de80b2829b813973080252e6d9b951f
Aliaksandr Radzivanovich <aradzivanovich@gmail.com>
2025-06-10 10:58:33 +0200

    wip

--- src/math/tvector.cpp
+++ src/math/tvector.cpp
@@ -28,239 +28,240 @@
 namespace qucs {
 
 // Returns the tvector element at the given position.
-template <class nr_type_t>
-nr_type_t tvector<nr_type_t>::get (int i) {
-  return data.at(i);
-}
+template <class nr_type_t> nr_type_t tvector<nr_type_t>::get(int i) { return data.at(i); }
 
 // Sets the tvector element at the given position.
-template <class nr_type_t>
-void tvector<nr_type_t>::set (int i, nr_type_t z) {
-  data.at(i) = z;
-}
+template <class nr_type_t> void tvector<nr_type_t>::set(int i, nr_type_t z) { data.at(i) = z; }
 
 // Sets all the tvector elements to the given value.
-template <class nr_type_t>
-void tvector<nr_type_t>::set (nr_type_t z) {
-  for (std::size_t i = 0; i < data.size (); i++)
+template <class nr_type_t> void tvector<nr_type_t>::set(nr_type_t z) {
+  for (std::size_t i = 0; i < data.size(); i++) {
     data[i] = z;
+  }
 }
 
 // Sets the specified tvector elements to the given value.
-template <class nr_type_t>
-void tvector<nr_type_t>::set (nr_type_t z, int start, int stop) {
-  for (std::size_t i = start; i < stop; i++)
+template <class nr_type_t> void tvector<nr_type_t>::set(nr_type_t z, int start, int stop) {
+  for (std::size_t i = start; i < stop; i++) {
     data[i] = z;
+  }
 }
 
 // Sets size to zero.  Does not reduce the capacity.
-template <class nr_type_t>
-void tvector<nr_type_t>::clear (void) {
-  data.clear ();
-  //size = 0;
-}
+template <class nr_type_t> void tvector<nr_type_t>::clear() { data.clear(); }
 
-/* The function returns the number of entries with the given value
-   deviating no more than the given epsilon. */
-template <class nr_type_t>
-int tvector<nr_type_t>::contains (nr_type_t val, double eps) {
+/* Returns the number of entries with the given value deviating no more than the given epsilon. */
+template <class nr_type_t> int tvector<nr_type_t>::contains(nr_type_t val, double eps) {
   int count = 0;
-  for (int i = 0; i < (int)data.size (); i++) if (abs ((data)[i] - val) <= eps) count++;
+  for (int i = 0; i < data.size(); i++) {
+    if (abs(data[i] - val) <= eps) {
+      count++;
+    }
+  }
   return count;
 }
 
 // Copies the specified elements from the given tvector.
-template <class nr_type_t>
-void tvector<nr_type_t>::set (tvector<nr_type_t> a, int start, int stop) {
-  for (int i = start; i < stop; i++) (*data)[i] = a.get (i);
+template <class nr_type_t> void tvector<nr_type_t>::set(tvector<nr_type_t> a, int start, int stop) {
+  for (int i = start; i < stop; i++) {
+    (*data)[i] = a.get(i);
+  }
 }
 
-// The function swaps the given rows with each other.
-template <class nr_type_t>
-void tvector<nr_type_t>::exchangeRows (int r1, int r2) {
-  assert (r1 >= 0 && r2 >= 0 && r1 < (int)data.size () && r2 < (int)data.size ());
-  nr_type_t s = (data)[r1];
-  (data)[r1] = (data)[r2];
-  (data)[r2] = s;
+// Swaps the given rows with each other.
+template <class nr_type_t> void tvector<nr_type_t>::exchangeRows(int r1, int r2) {
+  assert(r1 >= 0 && r2 >= 0 && r1 < data.size() && r2 < data.size());
+  nr_type_t s = data[r1];
+  data[r1] = data[r2];
+  data[r2] = s;
 }
 
 // Addition.
 template <class nr_type_t>
-tvector<nr_type_t> operator + (tvector<nr_type_t> a, tvector<nr_type_t> b) {
-  assert (a.size () == b.size ());
-  int n = a.size ();
-  tvector<nr_type_t> res (n);
-  for (int i = 0; i < n; i++) res.set (i, a.get (i) + b.get (i));
+tvector<nr_type_t> operator+(tvector<nr_type_t> a, tvector<nr_type_t> b) {
+  assert(a.size() == b.size());
+  tvector<nr_type_t> res((a.size()));
+  for (int i = 0; i < a.size(); i++) {
+    res.set(i, a.get(i) + b.get(i));
+  }
   return res;
 }
 
 // Intrinsic vector addition.
-template <class nr_type_t>
-tvector<nr_type_t> tvector<nr_type_t>::operator += (tvector<nr_type_t> a) {
-  assert (a.getSize () == (int)data.size ());
-  std::vector<nr_type_t> * src = a.getData ();
-  std::vector<nr_type_t> * dst = data;
-  for (int i = 0; i < (int)data.size (); i++) (*dst)[i] += (*src)[i];
+template <class nr_type_t> tvector<nr_type_t> tvector<nr_type_t>::operator+=(tvector<nr_type_t> a) {
+  assert(a.size() == data.size());
+  std::vector<nr_type_t> *src = a.getData();
+  std::vector<nr_type_t> *dst = data;
+  for (int i = 0; i < data.size(); i++) {
+    (*dst)[i] += (*src)[i];
+  }
   return *this;
 }
 
 // Subtraction.
 template <class nr_type_t>
-tvector<nr_type_t> operator - (tvector<nr_type_t> a, tvector<nr_type_t> b) {
-  assert (a.size () == b.size ());
-  int n = a.size ();
-  tvector<nr_type_t> res (n);
-  for (int i = 0; i < n; i++) res.set (i, a.get (i) - b.get (i));
+tvector<nr_type_t> operator-(tvector<nr_type_t> a, tvector<nr_type_t> b) {
+  assert(a.size() == b.size());
+  tvector<nr_type_t> res((a.size()));
+  for (int i = 0; i < a.size(); i++) {
+    res.set(i, a.get(i) - b.get(i));
+  }
   return res;
 }
 
 // Intrinsic vector subtraction.
-template <class nr_type_t>
-tvector<nr_type_t> tvector<nr_type_t>::operator -= (tvector<nr_type_t> a) {
-  assert (a.size () == (int)data.size ());
-  std::vector<nr_type_t> * src = a.getData ();
-  std::vector<nr_type_t> * dst = data;
-  for (int i = 0; i < (int)data.size (); i++) (*dst)[i] -= (*src)[i];
+template <class nr_type_t> tvector<nr_type_t> tvector<nr_type_t>::operator-=(tvector<nr_type_t> a) {
+  assert(a.size() == data.size());
+  std::vector<nr_type_t> *src = a.getData();
+  std::vector<nr_type_t> *dst = data;
+  for (int i = 0; i < data.size(); i++) {
+    (*dst)[i] -= (*src)[i];
+  }
   return *this;
 }
 
 // Intrinsic scalar multiplication.
-template <class nr_type_t>
-tvector<nr_type_t> tvector<nr_type_t>::operator *= (double s) {
-  std::vector<nr_type_t> * dst = data;
-  for (int i = 0; i < (int)data.size (); i++) (*dst)[i] *= s;
+template <class nr_type_t> tvector<nr_type_t> tvector<nr_type_t>::operator*=(double s) {
+  std::vector<nr_type_t> *dst = data;
+  for (int i = 0; i < data.size(); i++) {
+    (*dst)[i] *= s;
+  }
   return *this;
 }
 
 // Intrinsic scalar division.
-template <class nr_type_t>
-tvector<nr_type_t> tvector<nr_type_t>::operator /= (double s) {
-  std::vector<nr_type_t> * dst = data;
-  for (int i = 0; i < (int)data.size (); i++) (*dst)[i] /= s;
+template <class nr_type_t> tvector<nr_type_t> tvector<nr_type_t>::operator/=(double s) {
+  std::vector<nr_type_t> *dst = data;
+  for (int i = 0; i < data.size(); i++) {
+    (*dst)[i] /= s;
+  }
   return *this;
 }
 
 // Scalar multiplication.
-template <class nr_type_t>
-tvector<nr_type_t> operator * (double s, tvector<nr_type_t> a) {
-  int n = a.size ();
-  tvector<nr_type_t> res (n);
-  for (int i = 0; i < n; i++) res.set (i, s * a.get (i));
+template <class nr_type_t> tvector<nr_type_t> operator*(double s, tvector<nr_type_t> a) {
+  tvector<nr_type_t> res((a.size()));
+  for (int i = 0; i < a.size(); i++) {
+    res.set(i, s * a.get(i));
+  }
   return res;
 }
 
-template <class nr_type_t>
-tvector<nr_type_t> operator * (tvector<nr_type_t> a, double s) {
+template <class nr_type_t> tvector<nr_type_t> operator*(tvector<nr_type_t> a, double s) {
   return s * a;
 }
 
 // Vector multiplication (element by element).
 template <class nr_type_t>
-tvector<nr_type_t> operator * (tvector<nr_type_t> a, tvector<nr_type_t> b) {
-  assert (a.size () == b.size ());
-  int n = a.size ();
-  tvector<nr_type_t> res (n);
-  for (int i = 0; i < n; i++) res.set (i, a.get (i) * b.get (i));
+tvector<nr_type_t> operator*(tvector<nr_type_t> a, tvector<nr_type_t> b) {
+  assert(a.size() == b.size());
+  tvector<nr_type_t> res((a.size()));
+  for (int i = 0; i < a.size(); i++) {
+    res.set(i, a.get(i) * b.get(i));
+  }
   return res;
 }
 
 // Computes the scalar product of two vectors.
-template <class nr_type_t>
-nr_type_t scalar (tvector<nr_type_t> a, tvector<nr_type_t> b) {
-  assert (a.size () == b.size ());
+template <class nr_type_t> nr_type_t scalar(tvector<nr_type_t> a, tvector<nr_type_t> b) {
+  assert(a.size() == b.size());
   nr_type_t n = 0;
-  for (int i = 0; i < a.size (); i++) n += a.get (i) * b.get (i);
+  for (int i = 0; i < a.size(); i++) {
+    n += a.get(i) * b.get(i);
+  }
   return n;
 }
 
 // Constant assignment operation.
-template <class nr_type_t>
-tvector<nr_type_t> tvector<nr_type_t>::operator = (const nr_type_t val) {
-  for (int i = 0; i < (int)data.size (); i++) (*data)[i] = val;
+template <class nr_type_t> tvector<nr_type_t> tvector<nr_type_t>::operator=(const nr_type_t val) {
+  for (int i = 0; i < data.size(); i++) {
+    (*data)[i] = val;
+  }
   return *this;
 }
 
 // Returns the sum of the vector elements.
-template <class nr_type_t>
-nr_type_t sum (tvector<nr_type_t> a) {
+template <class nr_type_t> nr_type_t sum(tvector<nr_type_t> a) {
   nr_type_t res = 0;
-  for (int i = 0; i < a.size (); i++) res += a.get (i);
+  for (int i = 0; i < a.size(); i++) {
+    res += a.get(i);
+  }
   return res;
 }
 
 // Vector negation.
-template <class nr_type_t>
-tvector<nr_type_t> operator - (tvector<nr_type_t> a) {
-  int n = a.size ();
-  tvector<nr_type_t> res (n);
-  for (int i = 0; i < n; i++) res.set (i, -a.get (i));
+template <class nr_type_t> tvector<nr_type_t> operator-(tvector<nr_type_t> a) {
+  tvector<nr_type_t> res((a.size()));
+  for (int i = 0; i < a.size(); i++) {
+    res.set(i, -a.get(i));
+  }
   return res;
 }
 
 // Vector less comparison.
-template <class nr_type_t>
-bool operator < (tvector<nr_type_t> a, tvector<nr_type_t> b) {
-  assert (a.size () == b.size ());
-  int n = a.size ();
-  for (int i = 0; i < n; i++) if (a.get (i) >= b.get (i)) return false;
+template <class nr_type_t> bool operator<(tvector<nr_type_t> a, tvector<nr_type_t> b) {
+  assert(a.size() == b.size());
+  for (int i = 0; i < a.size(); i++) {
+    if (a.get(i) >= b.get(i)) {
+      return false;
+    }
+  }
   return true;
 }
 
 // Vector greater comparison.
-template <class nr_type_t>
-bool operator > (tvector<nr_type_t> a, tvector<nr_type_t> b) {
-  assert (a.size () == b.size ());
-  int n = a.size ();
-  for (int i = 0; i < n; i++) if (a.get (i) <= b.get (i)) return false;
+template <class nr_type_t> bool operator>(tvector<nr_type_t> a, tvector<nr_type_t> b) {
+  assert(a.size() == b.size());
+  for (int i = 0; i < a.size(); i++) {
+    if (a.get(i) <= b.get(i)) {
+      return false;
+    }
+  }
   return true;
 }
 
 // Scalar addition.
-template <class nr_type_t>
-tvector<nr_type_t> operator + (nr_type_t s, tvector<nr_type_t> a) {
-  int n = a.size ();
-  tvector<nr_type_t> res (n);
-  for (int i = 0; i < n; i++) res.set (i, s + a.get (i));
+template <class nr_type_t> tvector<nr_type_t> operator+(nr_type_t s, tvector<nr_type_t> a) {
+  tvector<nr_type_t> res((a.size()));
+  for (int i = 0; i < a.size(); i++) {
+    res.set(i, s + a.get(i));
+  }
   return res;
 }
 
-template <class nr_type_t>
-tvector<nr_type_t> operator + (tvector<nr_type_t> a, nr_type_t s) {
+template <class nr_type_t> tvector<nr_type_t> operator+(tvector<nr_type_t> a, nr_type_t s) {
   return s + a;
 }
 
 // Mean square norm.
-template <class nr_type_t>
-double norm (tvector<nr_type_t> a) {
+template <class nr_type_t> double norm(tvector<nr_type_t> a) {
 #if 0
   double k = 0;
   for (int i = 0; i < a.size (); i++) k += norm (a.get (i));
   return n;
 #else
   double scale = 0, n = 1, x, ax;
-  for (int i = 0; i < a.size (); i++) {
-    if ((x = real (a (i))) != 0) {
-      ax = fabs (x);
+  for (int i = 0; i < a.size(); i++) {
+    if ((x = real(a(i))) != 0) {
+      ax = fabs(x);
       if (scale < ax) {
-	x = scale / ax;
-	n = 1 + n * x * x;
-	scale = ax;
-      }
-      else {
-	x = ax / scale;
-	n += x * x;
+        x = scale / ax;
+        n = 1 + n * x * x;
+        scale = ax;
+      } else {
+        x = ax / scale;
+        n += x * x;
       }
     }
-    if ((x = imag (a (i))) != 0) {
-      ax = fabs (x);
+    if ((x = imag(a(i))) != 0) {
+      ax = fabs(x);
       if (scale < ax) {
-	x = scale / ax;
-	n = 1 + n * x * x;
-	scale = ax;
-      }
-      else {
-	x = ax / scale;
-	n += x * x;
+        x = scale / ax;
+        n = 1 + n * x * x;
+        scale = ax;
+      } else {
+        x = ax / scale;
+        n += x * x;
       }
     }
   }
@@ -269,47 +270,48 @@ double norm (tvector<nr_type_t> a) {
 }
 
 // Maximum norm.
-template <class nr_type_t>
-double maxnorm (tvector<nr_type_t> a) {
-  double nMax = 0, n;
-  for (int i = 0; i < a.size (); i++) {
-    n = norm (a.get (i));
-    if (n > nMax) nMax = n;
+template <class nr_type_t> double maxnorm(tvector<nr_type_t> a) {
+  double nMax = 0;
+  for (int i = 0; i < a.size(); i++) {
+    double n = norm(a.get(i));
+    if (n > nMax) {
+      nMax = n;
+    }
   }
   return nMax;
 }
 
 // Conjugate vector.
-template <class nr_type_t>
-tvector<nr_type_t> conj (tvector<nr_type_t> a) {
-  int n = a.size ();
-  tvector<nr_type_t> res (n);
-  for (int i = 0; i < n; i++) res.set (i, conj (a.get (i)));
+template <class nr_type_t> tvector<nr_type_t> conj(tvector<nr_type_t> a) {
+  tvector<nr_type_t> res((a.size()));
+  for (int i = 0; i < a.size(); i++) {
+    res.set(i, conj(a.get(i)));
+  }
   return res;
 }
 
 // Checks validity of vector.
-template <class nr_type_t>
-int tvector<nr_type_t>::isFinite (void) {
-  for (int i = 0; i < (int)data.size (); i++)
-    if (!std::isfinite (real ((*data)[i]))) return 0;
+template <class nr_type_t> int tvector<nr_type_t>::isFinite() {
+  for (int i = 0; i < data.size(); i++) {
+    if (!std::isfinite(real((*data)[i]))) {
+      return 0;
+    }
+  }
   return 1;
 }
 
-// The functions reorders the vector according to the given index array.
-template <class nr_type_t>
-void tvector<nr_type_t>::reorder (int * idx) {
+// Reorders the vector according to the given index array.
+template <class nr_type_t> void tvector<nr_type_t>::reorder(int *idx) {
   tvector<nr_type_t> old = *this;
-  for (int i = 0; i < (int)data.size (); i++) (*data)[i] = old.get (idx[i]);
+  for (int i = 0; i < data.size(); i++) {
+    (*data)[i] = old.get(idx[i]);
+  }
 }
 
 #ifdef DEBUG
-// Debug function: Prints the vector object.
-template <class nr_type_t>
-void tvector<nr_type_t>::print (void) {
-  for (int r = 0; r < (int)data.size (); r++) {
-    fprintf (stderr, "%+.2e%+.2ei\n", (double) real (get (r)),
-	     (double) imag (get (r)));
+template <class nr_type_t> void tvector<nr_type_t>::print() {
+  for (int r = 0; r < data.size(); r++) {
+    fprintf(stderr, "%+.2e%+.2ei\n", (double)real(get(r)), (double)imag(get(r)));
   }
 }
 #endif /* DEBUG */
--- src/math/tvector.h
+++ src/math/tvector.h
@@ -22,114 +22,87 @@
 #ifndef __TVECTOR_H__
 #define __TVECTOR_H__
 
-#include <vector>
 #include <limits>
+#include <vector>
 
 namespace qucs {
 
-template <class nr_type_t>
-class tvector;
+template <class nr_type_t> class tvector;
 
-// Forward declarations of friend functions.
-template <class nr_type_t>
-nr_type_t   scalar (tvector<nr_type_t>, tvector<nr_type_t>);
-template <class nr_type_t>
-double maxnorm (tvector<nr_type_t>);
-template <class nr_type_t>
-double norm (tvector<nr_type_t>);
-template <class nr_type_t>
-nr_type_t   sum (tvector<nr_type_t>);
-template <class nr_type_t>
-tvector<nr_type_t> conj (tvector<nr_type_t>);
-template <class nr_type_t>
-tvector<nr_type_t> operator + (tvector<nr_type_t>, tvector<nr_type_t>);
-template <class nr_type_t>
-tvector<nr_type_t> operator + (tvector<nr_type_t>, nr_type_t);
-template <class nr_type_t>
-tvector<nr_type_t> operator + (nr_type_t, tvector<nr_type_t>);
-template <class nr_type_t>
-tvector<nr_type_t> operator - (tvector<nr_type_t>, tvector<nr_type_t>);
-template <class nr_type_t>
-tvector<nr_type_t> operator * (tvector<nr_type_t>, double);
-template <class nr_type_t>
-tvector<nr_type_t> operator * (double, tvector<nr_type_t>);
-template <class nr_type_t>
-tvector<nr_type_t> operator * (tvector<nr_type_t>, tvector<nr_type_t>);
-template <class nr_type_t>
-tvector<nr_type_t> operator - (tvector<nr_type_t>);
-template <class nr_type_t>
-bool operator < (tvector<nr_type_t>, tvector<nr_type_t>);
-template <class nr_type_t>
-bool operator > (tvector<nr_type_t>, tvector<nr_type_t>);
+template <class nr_type_t> nr_type_t scalar(tvector<nr_type_t>, tvector<nr_type_t>);
+template <class nr_type_t> double maxnorm(tvector<nr_type_t>);
+template <class nr_type_t> double norm(tvector<nr_type_t>);
+template <class nr_type_t> nr_type_t sum(tvector<nr_type_t>);
+template <class nr_type_t> tvector<nr_type_t> conj(tvector<nr_type_t>);
+template <class nr_type_t> tvector<nr_type_t> operator+(tvector<nr_type_t>, tvector<nr_type_t>);
+template <class nr_type_t> tvector<nr_type_t> operator+(tvector<nr_type_t>, nr_type_t);
+template <class nr_type_t> tvector<nr_type_t> operator+(nr_type_t, tvector<nr_type_t>);
+template <class nr_type_t> tvector<nr_type_t> operator-(tvector<nr_type_t>, tvector<nr_type_t>);
+template <class nr_type_t> tvector<nr_type_t> operator*(tvector<nr_type_t>, double);
+template <class nr_type_t> tvector<nr_type_t> operator*(double, tvector<nr_type_t>);
+template <class nr_type_t> tvector<nr_type_t> operator*(tvector<nr_type_t>, tvector<nr_type_t>);
+template <class nr_type_t> tvector<nr_type_t> operator-(tvector<nr_type_t>);
+template <class nr_type_t> bool operator<(tvector<nr_type_t>, tvector<nr_type_t>);
+template <class nr_type_t> bool operator>(tvector<nr_type_t>, tvector<nr_type_t>);
 
-template <class nr_type_t>
-class tvector
-{
- public:
-  tvector () = default;
-  tvector (const std::size_t i) : data(i) {};
-  tvector (const tvector &) = default;
-  ~tvector () = default;
-  nr_type_t get (int);
-  void set (int, nr_type_t);
-  void set (nr_type_t);
-  void set (nr_type_t, int, int);
-  void set (tvector, int, int);
-  std::size_t  size (void) const { return data.size (); }
-  nr_type_t * getData (void) { return data.data(); }
-  void clear (void);
-  void exchangeRows (int, int);
-  int  isFinite (void);
-  void print (void);
-  void reorder (int *);
-  int  contains (nr_type_t, double eps = std::numeric_limits<double>::epsilon());
+template <class nr_type_t> class tvector {
+public:
+  tvector() = default;
+  explicit tvector(const std::size_t i) : data(i) {}
+  tvector(const tvector &) = default;
+  ~tvector() = default;
+  nr_type_t get(int);
+  void set(int, nr_type_t);
+  void set(nr_type_t);
+  void set(nr_type_t, int, int);
+  void set(tvector, int, int);
+  std::size_t size() const { return data.size(); }
+  nr_type_t *getData() { return data.data(); }
+  void clear();
+  void exchangeRows(int, int);
+  int isFinite();
+  void print();
+  void reorder(int *);
+  int contains(nr_type_t, double eps = std::numeric_limits<double>::epsilon());
 
   // some basic vector operations
-  friend tvector operator +<> (tvector, tvector);
-  friend tvector operator -<> (tvector, tvector);
-  friend tvector operator *<> (tvector, double);
-  friend tvector operator *<> (double, tvector);
-  friend tvector operator *<> (tvector, tvector);
-  friend tvector operator -<> (tvector);
-  friend tvector operator +<> (tvector, nr_type_t);
-  friend tvector operator +<> (nr_type_t, tvector);
+  friend tvector operator+ <>(tvector, tvector);
+  friend tvector operator- <>(tvector, tvector);
+  friend tvector operator* <>(tvector, double);
+  friend tvector operator* <>(double, tvector);
+  friend tvector operator* <>(tvector, tvector);
+  friend tvector operator- <>(tvector);
+  friend tvector operator+ <>(tvector, nr_type_t);
+  friend tvector operator+ <>(nr_type_t, tvector);
 
   // other operations
-  friend double norm<> (tvector);
-  friend double maxnorm<> (tvector);
-  friend nr_type_t   sum<> (tvector);
-  friend nr_type_t   scalar<> (tvector, tvector);
-  friend tvector     conj<> (tvector);
+  friend double norm<>(tvector);
+  friend double maxnorm<>(tvector);
+  friend nr_type_t sum<>(tvector);
+  friend nr_type_t scalar<>(tvector, tvector);
+  friend tvector conj<>(tvector);
 
   // comparisons
-  friend bool operator < <> (tvector, tvector);
-  friend bool operator > <> (tvector, tvector);
+  friend bool operator< <>(tvector, tvector);
+  friend bool operator><>(tvector, tvector);
 
   // intrinsic operators
-  tvector operator += (tvector);
-  tvector operator -= (tvector);
-  tvector operator *= (double);
-  tvector operator /= (double);
+  tvector operator+=(tvector);
+  tvector operator-=(tvector);
+  tvector operator*=(double);
+  tvector operator/=(double);
 
   // assignment operators
-  tvector operator = (const nr_type_t);
+  tvector operator=(const nr_type_t);
 
   // easy accessor operators
-  nr_type_t  operator () (int i) const {
-    return data.at(i);
-  }
-  nr_type_t& operator () (int i) {
-    return data.at(i); }
-   nr_type_t  operator [] (int i) const {
-    return data[i];
-  }
-  nr_type_t& operator [] (int i) {
-    return data[i];
-  }
+  nr_type_t operator()(int i) const { return data.at(i); }
+  nr_type_t &operator()(int i) { return data.at(i); }
+  nr_type_t operator[](int i) const { return data[i]; }
+  nr_type_t &operator[](int i) { return data[i]; }
 
- protected:
+protected:
   std::vector<nr_type_t> data;
-
 };
 
 } // namespace qucs
